7fa2a59ecd218fe1f03704bd5193442a49c50c2c
dwijkarimah <50562189+dwijkarimah@users.noreply.github.com>
2021-03-18 14:45:34 +0700

    Update soal1.js

--- tugas pertemuan 6-Looping/soal1.js
+++ tugas pertemuan 6-Looping/soal1.js
@@ -1,6 +1,6 @@
 let userAwal = 1;
 let banyakUser = 100;
 
-for (userAwal; userAwal <= banyakUser; userAwal++) {
+for (let userAwal=1; userAwal <=100; userAwal++) {
     document.write('User ke - ' + userAwal + '<br>');
 }
